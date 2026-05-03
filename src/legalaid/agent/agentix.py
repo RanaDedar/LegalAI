bccfe5d22c760ca8007fe5e206987580a648f578
Rana Dedar <rajpotdedar@gmail.com>
2026-05-03 21:28:21 +0500

    instructions refined

--- src/legalaid/agent/agentix.py
+++ src/legalaid/agent/agentix.py
@@ -12,7 +12,11 @@ vector_id = os.environ.get("VECTOR_STORE_ID")
 _vector_ids = [vector_id] if vector_id else []
 ppc_agent=Agent(
     name="LegalAId",
-    instructions="""you are a helpful Legal advisor and you have to always use the tool to answer the querry""",
+    instructions="""you are a helpful Legal advisor agent and you have to always use the tool to answer the querry
+    and according to the PPC and only according to Pakistan law
+    you have to meantion the sort of act related to that crime
+    they should be like related to crime 
+    related act :: punishment""",
     tools=[FileSearchTool(
         max_num_results=4,
         vector_store_ids=_vector_ids,
@@ -24,9 +28,7 @@ simple_agent=Agent(
     name="Simp",
     instructions="""you are a helpful assisstant and you have to answer his querries
     but if he asks about any sort of crime you have to handsoff to the PPC_agent for any queeries related to pakistan penal code
-    you have to meantion the sort of act related to that crime
-    they should be like related to crime 
-    related act :: punishment""",
+    """,
     model="gpt-5.4-nano",
     handoffs=[ppc_agent]
 )
